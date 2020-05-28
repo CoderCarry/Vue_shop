f4cb1f830a6de2485f3d46f6ade229baa426c194
彭崇矩 <571301185@qq.com>
2020-05-28 15:16:28 +0800

    主页、用户列表

--- src/main.js
+++ src/main.js
@@ -9,6 +9,15 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 设置一个拦截器 在挂载之前
+axios.interceptors.request.use(config => {
+  console.log(config);
+  // 为请求头对象添加上 token 验证的 authorization 字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 固定写法 必须 return config
+  return config
+})
+
 // 全局挂载
 // 这样每个vue组件都可以直接通过 this 访问到 $http ，从而直接发送ajax请求
 Vue.prototype.$http = axios
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -8,13 +8,21 @@ import Vue from 'vue'
 // // 导入弹框提示组件
 // import { Message } from 'element-ui'
 
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup } from 'element-ui'
 
 // 注册
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
 
 // 全局挂载
 Vue.prototype.$message = Message
